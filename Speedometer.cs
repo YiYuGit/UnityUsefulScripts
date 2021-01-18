25b05510f80c27cbfafb5cde727734f561539961
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-01-17 19:55:17 -0500

    gameObject speedometer
    This script measure the speed of gameobject with a rigidbody(like car, truck, plane...)  in MPH or KPH and show the speed via canvas text

---
+++ Speedometer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speedometer : MonoBehaviour
+{
+
+    //public GameObject vehicle;
+    public Rigidbody vehicle;
+    public Text speedText;
+    private int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody>(vehicle);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // MPH
+        speed = Convert.ToInt32(vehicle.velocity.magnitude * 2.237);
+        speedText.text = "E-Scooter Speed: " + speed + " MPH";
+
+        // KPH
+        //speed = Convert.ToInt32(rb.velocity.magnitude * 3.6);
+        //speedText.text = "E-Scooter Speed: " + speed + " KPH";
+
+
+
+    }
+}
