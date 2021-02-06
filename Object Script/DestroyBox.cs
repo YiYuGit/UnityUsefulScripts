034df5cb962e2f57c3f4968ca128378d56254c52
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-02-06 17:53:10 -0500

    Add files via upload

---
+++ Object Script/DestroyBox.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Partially come from https://www.youtube.com/watch?v=EgNV0PWVaS8
+/// Attach this to gameobject, and put the shattered version prefab of the same gameobject into the slot
+/// On collision or collide with object with certain tag
+/// The old object will be destroyed and instantiate a new shattered object at the same transform
+/// The effect is object get destroyed and break into pieces.
+/// All objects should have rigidbody and colliders
+
+/// </summary>
+public class DestroyBox : MonoBehaviour
+{
+    // The shattered verstion of the same object, ususally should be several pieces under one empty gameobjects
+    public GameObject ShatteredObject;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // This one is checking the magnitude of collision
+        if(other.relativeVelocity.magnitude > 5)
+        {
+            Instantiate(ShatteredObject, transform.position, transform.rotation);
+            Destroy(gameObject);
+
+        }
+    }
+
+
+
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        // This one is checking the tag of other gameobject
+        if (other.gameObject.CompareTag("Hammer"))
+        {
+            Instantiate(ShatteredObject, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+
+    }
+
+    // On mouse down is another option
+    /*
+    private void OnMouseDown()
+    {
+        Instantiate(ShatteredObject, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+    */
+}
