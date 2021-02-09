ba3b3ef3bc4f83f529595b7d3678a2e2749ac337
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-02-09 15:11:59 -0500

    Add files via upload

---
+++ Scene Management Script/TouchToRestartScene.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script is to restart the current scene when user touch the attached object trigger
+/// or 
+/// UI use the ClickRestartButton()
+/// or 
+/// pressing the key
+/// </summary>
+
+public class TouchToRestartScene : MonoBehaviour
+{
+    // for restarting the scene during game, press L key
+    void Update()
+    {
+        if (Input.GetKeyDown("l"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+
+
+    public void ClickRestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+
+}
