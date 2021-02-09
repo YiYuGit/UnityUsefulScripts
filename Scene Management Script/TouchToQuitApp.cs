d7591a7e903b419fc9607e786d3b94e1e1171ac1
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-02-09 15:12:22 -0500

    Add files via upload

---
+++ Scene Management Script/TouchToQuitApp.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This script is to quit the current scene when user touch the attached object trigger
+/// or 
+/// UI use the ClickQuitAppButton()
+/// or 
+/// press the key
+/// </summary>
+
+public class TouchToQuitApp : MonoBehaviour
+{
+    // for Quiting the application during game, press Esc key
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+         // save any game data here
+         #if UNITY_EDITOR
+         // Application.Quit() does not work in the editor so
+         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+         UnityEditor.EditorApplication.isPlaying = false;
+         #else
+            Application.Quit();
+         #endif
+        }
+
+    }
+
+
+    // for quiting the application using UI button
+    public void ClickQuitAppButton()
+    {
+        // save any game data here
+        #if UNITY_EDITOR
+         // Application.Quit() does not work in the editor so
+         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+         UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    // for quiting the application using trigger
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // save any game data here
+        #if UNITY_EDITOR
+         // Application.Quit() does not work in the editor so
+         // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+         UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+        }
+    }
+
+
+}
