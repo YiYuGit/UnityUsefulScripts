c693cd8281195a61355ffe87110eb04878d5d1e0
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-02-10 14:25:14 -0500

    Edit

---
+++ Agent Script/AgentFollowUser.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentFollowUser : MonoBehaviour
+{
+    /// <summary>
+    /// Attach this script to agent. On start, all agents with this script will wait a random range time and then follow the player(user)
+    /// 
+    /// </summary>
+    /// 
+
+    // Put user here
+    public GameObject user;
+
+    Animator animator;
+
+    NavMeshAgent myNavMeshAgent;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+
+        StartCoroutine(RandomStartTime());
+
+    }
+
+
+    IEnumerator RandomStartTime()
+    {
+        // a random time between 0 and 1 second to make each agent walk different
+        float time = Random.Range(0.0f, 1.0f);
+
+        //yield on a new YieldInstruction that waits for random seconds.
+        yield return new WaitForSeconds(time);
+
+        //After waiting, the animator IsWalking become true.
+        animator.SetBool("IsWalking", true);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Update user position to the agent
+        myNavMeshAgent.SetDestination(user.transform.position);
+    }
+}
---
+++ Agent Script/NPCtext.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// This script is attached to NPC object, under NPC object, put the NPC text, drag and drop the text and player into the slot
+/// The script will detect the distance between NPC and player, when the distance is smaller than the approach distance, the text will change color and facing the player
+/// </summary>
+public class NPCtext : MonoBehaviour
+{
+    //The text object, remember to put the text Anchor to 'middle center', so the text rotate around the center
+    public GameObject text;
+
+    //Player object drop here
+    public GameObject player;
+
+    //Approaching distance between NPC and player
+    public float approachDist = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Turn off text on start
+        text.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate the distance between player and NPC
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+
+        // If text is not empty and the distance is smaller than the approach distance
+        if (text != null && dist < approachDist)
+        {
+            //Active the text
+            text.SetActive(true);
+
+            // Let the text facing the player, and change color, 
+            //text.transform.LookAt(player.transform.position);      // This option look at player x y z.
+
+            //This option only look at on Y axis to keep text straight 
+            Vector3 targetPostition = new Vector3(player.transform.position.x, text.transform.position.y, player.transform.position.z);
+            text.transform.LookAt(targetPostition);
+
+            // Rotate the text once moreto get correct view
+            text.transform.Rotate(0, 180, 0);
+
+            // Change the text color
+            text.GetComponent<TextMesh>().color = Color.blue;
+        }
+
+        if (text == null || dist > approachDist)
+        {
+            //If text is empty or player is far away, turn off text
+            text.SetActive(false);
+        }
+    }
+}
