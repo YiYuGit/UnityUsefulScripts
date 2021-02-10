e50760a7b095137dd54155d09925a7248d397440
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-02-10 14:49:01 -0500

    upload

---
+++ Agent Script/RandomGenerateAgent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach this to an empty object, so it will generate agent at certain location and time interval
+/// </summary>
+
+public class RandomGenerateAgent : MonoBehaviour
+{
+    //Put the agent prefab here
+    public GameObject agnet;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(generateAgent());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator generateAgent()
+    {
+        while (true)
+        {
+            //The time between instantiate
+            float seconds = Random.Range(2f, 5f);
+        
+            yield return new WaitForSeconds(3.0f);
+
+            // Select random location
+            float x = Random.Range(-14f, -2f);
+            float y = 0.4f;
+            float z = Random.Range(29f, 36f);
+            Vector3 location = new Vector3(x, y, z);
+
+            // Instantiate new object
+            GameObject p1 = Instantiate(agnet);
+
+
+            //Assign to new location
+            p1.transform.position = location;
+        }
+
+    }
+}
---
+++ Agent Script/SelfDestroy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach this script to any game object, and set the self destroy time
+/// The object will be destroyed at the set time
+/// And the remaining time will be displayed
+/// 
+/// </summary>
+public class SelfDestroy : MonoBehaviour
+{
+    // Set self destroy time
+    public float destroyTime = 100f;
+
+    [SerializeField]
+    private bool timerIsRunning = false;
+    [SerializeField]
+    private float timeRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Starts the timer and destroy count down on start
+        timerIsRunning = true;
+        Destroy(gameObject, destroyTime);
+        timeRemaining = destroyTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                //Debug.Log("Time has run out!");
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+}
