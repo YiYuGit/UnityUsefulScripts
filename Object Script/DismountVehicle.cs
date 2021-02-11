3574248c3c6a6fc26735e9c1645739868739b2d7
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-02-11 15:07:58 -0500

    Upload

---
+++ Object Script/DismountVehicle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attachi this script to an object with trigger and put at the location you want to collide with vehicle
+/// CameraRig is where you put player/VRCameraRig.When hte cameraRig is on the vehicle, the cameraRig's parenet is the vehicle
+/// On Trigger Enter, if the vehicle is the tag.
+/// The script will switch the camerarig parent to the LocationCube and change the camerarig location.
+/// In this way, the player dismount the vehicle
+/// 
+/// </summary>
+public class DismountVehicle : MonoBehaviour {
+
+    //Put the player and the dismount location here
+    public GameObject cameraRig;
+    public GameObject LocationCube;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("vehicle"))
+        {
+
+            cameraRig.transform.parent = LocationCube.transform;
+            cameraRig.transform.position= LocationCube.transform.position;
+            Debug.Log("cameraRig's Parent: " + cameraRig.transform.parent.name);
+        }
+    }
+}
---
+++ Object Script/TouchToInstantiateObj.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach this script to an object with trigger, on trigger enter, the script will instantiate at the 'location' position
+/// The 'location'object can be empty or with component.
+/// </summary>
+
+
+public class TouchToInstantiateObj : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject prefab;
+    public GameObject location;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("user"))
+        {
+            GameObject newItem = Object.Instantiate(prefab);
+            newItem.transform.position = location.transform.position;
+        }
+    }
+}
