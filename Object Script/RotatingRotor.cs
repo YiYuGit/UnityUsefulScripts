e16b164c5e7e669250bf7aabce18de8c71c7eb94
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-02-13 19:26:39 -0500

    Create RotatingRotor.cs

---
+++ Object Script/RotatingRotor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script is used to rotate rotor/propeller gameobject in helopter/plane
+/// attach the script to object, find the right axis and set the rotateSpeed
+/// </summary>
+
+
+public class RotatingRotor : MonoBehaviour {
+
+	//Rotate speed
+    public int rotateSpeed =1800;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(new Vector3(0, Time.deltaTime*rotateSpeed, 0));
+	}
+}
