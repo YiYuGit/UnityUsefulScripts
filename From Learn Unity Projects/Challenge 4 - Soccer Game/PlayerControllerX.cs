e154a71f379e6f40c265024958c6bc1c0e80f8c8
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-03-01 07:50:59 -0500

    upload

---
+++ From Learn Unity Projects/Challenge 4 - Soccer Game/PlayerControllerX.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The player have powerup and boost.
+/// The powerup increase the hitting force
+/// The boost increase the speed.
+/// The moving direction is determined by the focal point direction.
+/// The moving control is determined by the vertical input.
+/// </summary>
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    [SerializeField]
+    private float speed = 500;
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+
+    private float normalStrength = 10; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+
+    private ParticleSystem smokeParticle;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+
+        smokeParticle = GameObject.Find("Smoke_Particle").GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+
+        // Set Boost particale system position to beneath player
+        smokeParticle.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+
+        // The "Boost" status, when pressing space key.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            smokeParticle.Play();
+            speed *= 1.5f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            smokeParticle.Stop();
+            speed /= 1.5f;
+        }
+
+    }
+
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+
+            StartCoroutine(PowerupCooldown());
+            powerupIndicator.SetActive(true);
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position; 
+           
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ From Learn Unity Projects/Challenge 4 - Soccer Game/RotateCameraX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script rotoate the camera view around the player.
+/// </summary>
+public class RotateCameraX : MonoBehaviour
+{
+    private float speed = 200;
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
+
+        transform.position = player.transform.position; // Move focal point with player
+
+    }
+}
