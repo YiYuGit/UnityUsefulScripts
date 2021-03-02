53071ee9ac187f35aec8360d544dd18c5a962954
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-03-02 08:35:38 -0500

    upload

---
+++ From Learn Unity Projects/Challenge 5 - Whack A Food/DestroyObjectX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is attached to particle system of explosion.
+/// This will destroy the particle after 2 seconds.
+/// </summary>
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+}
---
+++ From Learn Unity Projects/Challenge 5 - Whack A Food/DifficultyButtonX.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Attached to difficulty button, and set int for different difficulty value
+/// 
+/// Similar to the DiffcultyButton in Challenge 5 - Clicky Crates
+/// 
+/// </summary>
+public class DifficultyButtonX : MonoBehaviour
+{
+    private Button button;
+    private GameManagerX gameManagerX;
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManagerX = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDifficulty);
+    }
+
+    /* When a button is clicked, call the StartGame() method
+     * and pass it the difficulty value (1, 2, 3) from the button 
+    */
+    void SetDifficulty()
+    {
+        Debug.Log(button.gameObject.name + " was clicked");
+        gameManagerX.StartGame(difficulty);
+    }
+
+
+
+}
