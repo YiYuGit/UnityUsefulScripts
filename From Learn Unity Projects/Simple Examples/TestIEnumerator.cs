b09efab5224757c463a106462e809eac5a096440
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-03-08 11:14:19 -0500

    Upload

---
+++ From Learn Unity Projects/Simple Examples/TestIEnumerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script will loop the Debug.Log part of IEnumerator
+/// </summary>
+
+public class TestIEnumerator : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(MyCoroutine());
+    }
+
+
+    private IEnumerator MyCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        Debug.Log("Test");
+        StartCoroutine(MyCoroutine());
+    }
+}
---
+++ From Learn Unity Projects/Simple Examples/TestSwitchCase.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script will read the dialogue content, find the case or default case then print the content.
+/// </summary>
+
+public class TestSwitchCase : MonoBehaviour
+{
+    int dialogue = 3;
+
+    void Start()
+    {
+        switch(dialogue)
+       
+        {
+      case 2:
+         print("Goodbye, old friend");
+            break;
+      case 1:
+         print("Hello there");
+            break;
+            default:
+         print("Incorrect dialogue value");
+            break;
+        }
+    }
+    
+}
