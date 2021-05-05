e989d128522ef3d7d054a705de4fb970cc748cae
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-05-05 18:12:45 -0400

    Update DetectCollisions.cs

--- From Learn Unity Projects/Prototype 2 - Feed Animal/DetectCollisions.cs
+++ From Learn Unity Projects/Prototype 2 - Feed Animal/DetectCollisions.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 
 /// <summary>
 /// Simple collision detection and will destroy both colliding objets.
-/// Remember to put trigger collider on both objects and at least one have rigidbody, 
+/// Remember to put trigger collider on both objects and at least one object should have rigidbody, 
 /// Uncheck use gravity if necessary
 /// </summary>
 
 public class DetectCollisions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter(Collider other)
     {
