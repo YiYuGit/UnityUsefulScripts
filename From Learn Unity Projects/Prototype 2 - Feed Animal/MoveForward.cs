bbcf8c78dca454cddffc31f331c6b0e0328b4c36
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-05-06 09:45:45 -0400

    Update MoveForward.cs

--- From Learn Unity Projects/Prototype 2 - Feed Animal/MoveForward.cs
+++ From Learn Unity Projects/Prototype 2 - Feed Animal/MoveForward.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 /// <summary>
 ///  Attach to something like a bullet, it will move forward at the public float speed.
 ///  Vector3.forward can be change to a specific vector.
+///  This is a simple script without using rigidbody.
 /// </summary>
 
 public class MoveForward : MonoBehaviour
 {
 
     public float speed = 50f;
+
     // Start is called before the first frame update
     void Start()
     {
