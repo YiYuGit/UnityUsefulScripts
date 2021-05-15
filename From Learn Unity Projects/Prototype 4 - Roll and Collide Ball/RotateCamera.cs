64d90d0bb737e7f92891b8dbeebfc53e90c7ab4c
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-05-15 17:36:17 -0400

    Update RotateCamera.cs

--- From Learn Unity Projects/Prototype 4 - Roll and Collide Ball/RotateCamera.cs
+++ From Learn Unity Projects/Prototype 4 - Roll and Collide Ball/RotateCamera.cs
@@ -10,11 +10,6 @@ public class RotateCamera : MonoBehaviour
 {
     public float rotationSpeed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
