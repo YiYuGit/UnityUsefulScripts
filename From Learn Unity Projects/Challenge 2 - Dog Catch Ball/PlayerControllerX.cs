7e2eaa39a9469c45c77741307351d88b248e32bf
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-06-05 16:30:54 -0400

    upload

---
+++ From Learn Unity Projects/Challenge 2 - Dog Catch Ball/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    private float cdTime = 1.0f;
+
+    private float timer = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update our timer 
+        timer += Time.deltaTime;
+
+        // On spacebar press, send dog
+        // if timer is up and spacebar press, send dog
+        if (timer > cdTime && Input.GetKeyDown(KeyCode.Space))
+        { 
+           Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            timer = 0.0f;
+        }
+    }
+}
