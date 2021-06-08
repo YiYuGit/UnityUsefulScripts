62d68e3047e4a904cf93bc9600d12bdcc1e724c9
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-06-07 20:22:11 -0400

    Update DetectCollisionsX.cs

--- From Learn Unity Projects/Challenge 2 - Dog Catch Ball/DetectCollisionsX.cs
+++ From Learn Unity Projects/Challenge 2 - Dog Catch Ball/DetectCollisionsX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DetectCollisionsX : MonoBehaviour
 {
-
+    //This script destroy object when triggered
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
