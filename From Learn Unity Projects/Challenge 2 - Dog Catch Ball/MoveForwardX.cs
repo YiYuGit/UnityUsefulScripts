a313becbafba29e3227babe51a0e953ab2c6adc0
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-06-08 15:57:30 -0400

    Update MoveForwardX.cs

--- From Learn Unity Projects/Challenge 2 - Dog Catch Ball/MoveForwardX.cs
+++ From Learn Unity Projects/Challenge 2 - Dog Catch Ball/MoveForwardX.cs
@@ -6,7 +6,6 @@ public class MoveForwardX : MonoBehaviour
 {
     public float speed;
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
