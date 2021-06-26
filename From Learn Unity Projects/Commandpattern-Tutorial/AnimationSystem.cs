a1b09091e981ff84686a5d1e4447d672913b2f93
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-06-25 22:24:56 -0400

    Update AnimationSystem.cs

--- From Learn Unity Projects/Commandpattern-Tutorial/AnimationSystem.cs
+++ From Learn Unity Projects/Commandpattern-Tutorial/AnimationSystem.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Animation system script
+/// </summary>
+
 public class AnimationSystem
 {
     
