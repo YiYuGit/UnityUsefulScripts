2f62c40cfd2e09171c538357236d62f4735c56e1
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-06-28 20:14:59 -0400

    Update ExplosionController.cs

--- From Learn Unity Projects/ObserverDemo/ExplosionController.cs
+++ From Learn Unity Projects/ObserverDemo/ExplosionController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// When called KilleMe(), the script destroy the attached gameobject 
+/// </summary>
+
 public class ExplosionController : MonoBehaviour
 {
     public void KillMe()
