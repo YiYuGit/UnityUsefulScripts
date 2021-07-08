f583731fa8565d93efc286344ebd4cfcd21f3be0
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-07-08 18:27:17 -0400

    Update Game.cs

--- From Learn Unity Projects/Programming Systems and Architecture/1. Creating Properties/Game.cs
+++ From Learn Unity Projects/Programming Systems and Architecture/1. Creating Properties/Game.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Game class
+/// </summary>
+
 public class Game  MonoBehaviour
 {
     void Start()
     {
         Player myPlayer = new Player();
 
-        Properties can be used just like variables
+        // Properties can be used just like variables
         myPlayer.Experience = 5;
         int x = myPlayer.Experience;
     }
