5d37afa8d912ec9a0329a688d40a9649f13ac2bd
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-07-19 12:14:44 -0400

    Update GenericClass.cs

--- From Learn Unity Projects/Programming Systems and Architecture/5. Generics/GenericClass.cs
+++ From Learn Unity Projects/Programming Systems and Architecture/5. Generics/GenericClass.cs
@@ -4,6 +4,7 @@ using System.Collections;
 //Here is a generic class. Notice the generic type 'T'.
 //'T' will be replaced with an actual type, as will also 
 //instances of the type 'T' used in the class.
+
 public class GenericClass<T>
 {
     T item;
