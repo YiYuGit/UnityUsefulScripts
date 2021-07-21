1eb6c3b0555c1267144d5807f1cbe3f3bca6b387
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-07-21 19:48:55 -0400

    Update SomeClass.cs

--- From Learn Unity Projects/Programming Systems and Architecture/5. Generics/SomeClass.cs
+++ From Learn Unity Projects/Programming Systems and Architecture/5. Generics/SomeClass.cs
@@ -6,6 +6,7 @@ public class SomeClass
     //Here is a generic method. Notice the generic
     //type 'T'. This 'T' will be replaced at runtime
     //with an actual type. 
+
     public T GenericMethod<T>(T param)
     {
         return param;
