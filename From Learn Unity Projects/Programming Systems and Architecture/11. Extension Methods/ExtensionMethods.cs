8d9f4b000fd7f5fbe96bb204d1d9449d6725451a
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-08-09 20:28:37 -0400

    Update ExtensionMethods.cs

--- From Learn Unity Projects/Programming Systems and Architecture/11. Extension Methods/ExtensionMethods.cs
+++ From Learn Unity Projects/Programming Systems and Architecture/11. Extension Methods/ExtensionMethods.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 //It is common to create a class to contain all of your
 //extension methods. This class must be static.
+
 public static class ExtensionMethods
 {
     //Even though they are used like normal methods, extension
