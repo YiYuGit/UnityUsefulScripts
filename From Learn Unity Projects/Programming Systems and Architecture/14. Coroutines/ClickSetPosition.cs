cd1c795418e034c6e779c298c82610a5414da006
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-08-15 08:35:31 -0400

    Update ClickSetPosition.cs

--- From Learn Unity Projects/Programming Systems and Architecture/14. Coroutines/ClickSetPosition.cs
+++ From Learn Unity Projects/Programming Systems and Architecture/14. Coroutines/ClickSetPosition.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// OnMouseDown if raycast hit a game object, take that hit point postion plus a vector3 to give to coroutineScript's target
+/// </summary>
 public class ClickSetPosition : MonoBehaviour
 {
     public PropertiesAndCoroutines coroutineScript;
