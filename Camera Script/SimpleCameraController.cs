23bf2e912d5bd2b696ec45f92f7ec218ec71cc4d
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-08-30 13:42:48 -0400

    Create SimpleCameraController.cs

---
+++ Camera Script/SimpleCameraController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System;
+
+/// <summary>
+/// Simple camera controller that use the Mouse X and Y to rotate the camera
+/// </summary>
+
+public class SimpleCameraController : MonoBehaviour
+{
+    Vector2 rotation = new Vector2(0, 0);
+    public float speed = 3;
+
+    void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * speed;
+    }
+}
