d60c4b214ffd0884d0c1f3f3ca0dd0b8f7500345
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-08-30 12:18:55 -0400

    Create ExplosionAnimation.cs

---
+++ Object Script/ExplosionAnimation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// In the Unity Learn2D project, when On Trigger Enter detect Player, set the explosion true and play the explo animation, then destroy the explosion obejct in 3 sec.
+/// </summary>
+
+
+public class ExplosionAnimation : MonoBehaviour
+{
+
+    public GameObject explosion;
+    Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = explosion.GetComponent<Animator>();
+        explosion.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            explosion.SetActive(true);
+            animator.SetBool("explo", true);
+            Destroy(explosion, 3);
+        }
+    }
+
+}
