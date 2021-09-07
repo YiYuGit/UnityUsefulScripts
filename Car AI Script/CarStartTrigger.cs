ef1dadc53953a6c962766d9f7823a5cc1e843cab
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-09-07 10:59:59 -0400

    Create CarStartTrigger.cs

---
+++ Car AI Script/CarStartTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Once triggered by user, the ca stopbox will be moved to the side, so the car can start drive
+/// </summary>
+
+
+public class CarStartTrigger : MonoBehaviour {
+
+	public GameObject stopbox;
+
+	// Use this for initialization
+	void Start () 
+	{
+		stopbox.SetActive(true);
+
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag ("user"))
+			transform.Translate(0f,0f,-1000f*Time.deltaTime);
+	}
+
+
+}
