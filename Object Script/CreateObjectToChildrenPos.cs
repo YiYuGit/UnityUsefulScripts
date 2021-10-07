1eb36fb8c667205e3132ad333f1beff425e021fa
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-10-07 19:32:42 -0400

    Update CreateObjectToChildrenPos.cs

--- Object Script/CreateObjectToChildrenPos.cs
+++ Object Script/CreateObjectToChildrenPos.cs
@@ -5,7 +5,7 @@ using UnityEngine;
     /// <summary>
     /// This script attach to a parent gameobject, and take a public gameobject 'a'. 
     /// On start, the script will create 'a's at all the children gameobject position and rotations.
-    /// This can be used when you need to create gameobject a certain location. 
+    /// This can be used when you need to create gameobjects at certain locations. 
     /// Empty gameobject can be used as place holder
     /// The place holder can be turned off (optional) after new object creation is done.
     /// </summary>
