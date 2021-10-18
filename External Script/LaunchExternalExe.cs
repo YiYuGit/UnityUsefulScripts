0c8620ee7111c687cdbde6cc978bf37989028b56
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-10-17 20:48:07 -0400

    Update LaunchExternalExe.cs

--- External Script/LaunchExternalExe.cs
+++ External Script/LaunchExternalExe.cs
@@ -4,20 +4,18 @@ using System.Diagnostics;
 using System;
 
 /// <summary>
-/// This script launch the external exe from Unity 
+/// This script can launch the external exe from Unity 
 /// </summary>
 
 
 public class LaunchExternalExe : MonoBehaviour
 {
 
-    // Path of the external exe, using notepad as example
+    // Path of the external exe, using the notepad as example
     
     [Header("Press key 'p' to launch the Exe ")]
     [Header("Exe Path")]
     public string path = "C:\\WINDOWS\\system32\\notepad.exe";
-
-
 
     // Start is called before the first frame update
     void Start()
