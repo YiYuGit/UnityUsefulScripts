55e19bc404aea01c7d13e49e6169779419f024f8
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-10-25 14:32:52 -0400

    Update RotatingRotor.cs

--- Object Script/RotatingRotor.cs
+++ Object Script/RotatingRotor.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 /// <summary>
 /// This script is used to rotate rotor/propeller gameobject in helopter/plane
 /// attach the script to object, find the right axis and set the rotateSpeed
+/// move the "Time.deltaTime*rotateSpeed" to the right position in the Vector3
 /// This is for self-rotating objects. For roatating wheels ,there are other scripts.
 /// </summary>
 
 
-public class RotatingRotor : MonoBehaviour {
+public class RotatingRotor : MonoBehaviour 
 
-	//Rotate speed
+{
+
+	//Rotate speed of the object
     public int rotateSpeed =1800;
 
 
