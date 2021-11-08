f9c6101b5835bc97da3869fd9cc74899c1b0c151
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-11-07 21:39:21 -0500

    Update TouchToInstantiateObj.cs

--- Object Script/TouchToInstantiateObj.cs
+++ Object Script/TouchToInstantiateObj.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Attach this script to an object with trigger, on trigger enter, the script will instantiate at the 'location' position
+/// Attach this script to an object with trigger, on trigger enter of user, the script will instantiate the prefab object at the 'location' position
 /// The 'location'object can be empty or with component.
 /// </summary>
 
@@ -11,7 +11,9 @@ using UnityEngine;
 public class TouchToInstantiateObj : MonoBehaviour {
 
     [SerializeField]
+    // Drop prefab here
     public GameObject prefab;
+    // Drop the location object here
     public GameObject location;
 
 
