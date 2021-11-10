1e46b927c3291b79c15a35afb9eba5f623e25b8b
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2021-11-10 17:18:10 -0500

    Update MiniMapFollowPlayer.cs

--- NPC Script/MiniMapFollowPlayer.cs
+++ NPC Script/MiniMapFollowPlayer.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// This tutorial is from https://www.youtube.com/watch?v=28JTTXqMvOU
+/// This script come from the tutorial of https://www.youtube.com/watch?v=28JTTXqMvOU
 /// 
-/// Steps to make a mini map:
+/// The steps to make a mini map:
 /// (1) Make a secondary camera, rename it MiniMapCamera, put it above player in scene, rotate it 90deg in x axis, so the camera look down to player vertically. The camera projection mode usually should be Othrographic
 /// (2) Make a new Canvas, inside it, make a new Raw Image, resize and reposition it on canvas. This will be the minimap's size and location on screen.
 /// (3) Make a new RenderTexture, resize it to fit the (2) Raw Image, and in MiniMapCamera drag and drop the RenderTexture in Target Texture. This will update the (2) raw image's texture in realtime.
