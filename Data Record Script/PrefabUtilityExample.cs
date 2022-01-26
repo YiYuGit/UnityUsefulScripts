84771570686f10482258405a218a808636a98c41
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2022-01-25 23:05:42 -0500

    Create PrefabUtilityExample.cs

---
+++ Data Record Script/PrefabUtilityExample.cs
@@ -0,0 +1,42 @@
+﻿// Create a folder (right click in the Assets directory, click Create>New Folder)
+// and name it “Editor” if one doesn’t exist already. Place this script in that folder.
+
+// https://docs.unity3d.com/ScriptReference/PrefabUtility.html
+// This script creates a new menu item Examples>Create Prefab in the main menu.
+// Use it to create Prefab(s) from the selected GameObject(s).
+// It will be placed in the root Assets folder.
+
+using UnityEngine;
+using UnityEditor;
+
+public class PrefabUtilityExample
+{
+    // Creates a new menu item 'Examples > Create Prefab' in the main menu.
+    [MenuItem("Examples/Create Prefab")]
+    static void CreatePrefab()
+    {
+        // Keep track of the currently selected GameObject(s)
+        GameObject[] objectArray = Selection.gameObjects;
+
+        // Loop through every GameObject in the array above
+        foreach (GameObject gameObject in objectArray)
+        {
+            // Set the path as within the Assets folder,
+            // and name it as the GameObject's name with the .Prefab format
+            string localPath = "Assets/" + gameObject.name + ".prefab";
+
+            // Make sure the file name is unique, in case an existing Prefab has the same name.
+            localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
+
+            // Create the new Prefab.
+            PrefabUtility.SaveAsPrefabAssetAndConnect(gameObject, localPath, InteractionMode.UserAction);
+        }
+    }
+
+    // Disable the menu item if no selection is in place.
+    [MenuItem("Examples/Create Prefab", true)]
+    static bool ValidateCreatePrefab()
+    {
+        return Selection.activeGameObject != null && !EditorUtility.IsPersistent(Selection.activeGameObject);
+    }
+}
