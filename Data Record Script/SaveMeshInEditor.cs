a61b744e377d5ab2b4510ae5d13c4815e41312c6
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2022-01-25 20:53:42 -0500

    Create SaveMeshInEditor.cs

---
+++ Data Record Script/SaveMeshInEditor.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+// Usage: Attach to gameobject, assign target gameobject (from where the mesh is taken), Run, Press savekey
+// https://unitycoder.com/blog/2013/01/26/save-mesh-created-by-script-in-editor-playmode/
+
+public class SaveMeshInEditor : MonoBehaviour
+{
+
+    public KeyCode saveKey = KeyCode.F12;
+    public string saveName = "SavedMesh";
+    public Transform selectedGameObject;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(saveKey))
+        {
+            SaveAsset();
+        }
+    }
+
+    void SaveAsset()
+    {
+        var mf = selectedGameObject.GetComponent<MeshFilter>();
+        if (mf)
+        {
+            var savePath = "Assets/" + saveName + ".asset";
+            Debug.Log("Saved Mesh to:" + savePath);
+            AssetDatabase.CreateAsset(mf.mesh, savePath);
+        }
+    }
+}
+#endif
