b2288e6941c0784f93e4c1fa610e4be45ea88f33
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2022-01-31 22:52:09 -0500

    Create RenameChildrenInEditor.cs

---
+++ Editor Tool/RenameChildrenInEditor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// From https://answers.unity.com/questions/1758798/is-there-anyway-to-batch-renaming-via-editor-not-o.html
+/// a custom editor window to rename children 
+/// To use this script:
+/// 1. Save the script in the project.
+/// 2. Go to GameObject/Rename children.
+/// 3. Select the parent of the objects to rename.
+/// 4. Fill the fields and press the button.
+/// </summary>
+public class RenameChildrenInEditor : EditorWindow
+{
+    private static readonly Vector2Int size = new Vector2Int(250, 100);
+    private string childrenPrefix;
+    private int startIndex;
+    [MenuItem("GameObject/Rename children")]
+    public static void ShowWindow()
+    {
+        EditorWindow window = GetWindow<RenameChildrenInEditor>();
+        window.minSize = size;
+        window.maxSize = size;
+    }
+    private void OnGUI()
+    {
+        childrenPrefix = EditorGUILayout.TextField("Children prefix", childrenPrefix);
+        startIndex = EditorGUILayout.IntField("Start index", startIndex);
+        if (GUILayout.Button("Rename children"))
+        {
+            GameObject[] selectedObjects = Selection.gameObjects;
+            for (int objectI = 0; objectI < selectedObjects.Length; objectI++)
+            {
+                Transform selectedObjectT = selectedObjects[objectI].transform;
+                for (int childI = 0, i = startIndex; childI < selectedObjectT.childCount; childI++) selectedObjectT.GetChild(childI).name = $"{childrenPrefix}{i++}";
+            }
+        }
+    }
+}
