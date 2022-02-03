719ec586c9fba534ce8d31d42c6442e7283cf30d
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2022-02-02 21:11:39 -0500

    Update CreatePrefabFromSelected.cs

--- Editor Tool/CreatePrefabFromSelected.cs
+++ Editor Tool/CreatePrefabFromSelected.cs
@@ -2,18 +2,16 @@
 using UnityEngine;
 
 /// <summary>
-/// This is a menu script.
+/// This is a menu script. This is from:
 /// https://unitycoder.com/blog/2013/01/26/save-mesh-created-by-script-in-editor-playmode/
 /// Creates a prefab from a selected game object. Can be used to save gameObject and mesh that are genenrated during gameplay into prefab and asset.
+/// Adds a menu named "Create Prefab From Selected" to the GameObject menu.
+/// This script can save the gameobject in play mode to a prefab and also make the mesh of this prefab into a mesh asset.
 /// </summary>
 class CreatePrefabFromSelected
 {
     const string menuName = "GameObject/Create Prefab From Selected";
 
-    /// <summary>
-    /// Adds a menu named "Create Prefab From Selected" to the GameObject menu.
-    /// This script can save the gameobject in play mode to a prefab and also make the mesh of this prefab into a mesh asset.
-    /// </summary>
     [MenuItem(menuName)]
     static void CreatePrefabMenu()
     {
@@ -31,11 +29,9 @@ class CreatePrefabFromSelected
 
     }
 
-    /// <summary>
-    /// Validates the menu.
-    /// The item will be disabled if no game object is selected.
-    /// </summary>
-    /// <returns>True if the menu item is valid.</returns>
+    // Validates the menu.
+    // The item will be disabled if no game object is selected.
+    // returns True if the menu item is valid.
     [MenuItem(menuName, true)]
     static bool ValidateCreatePrefabMenu()
     {
