9fedacabba6efacd459e41aa53fab0c570998147
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2022-12-12 11:55:12 -0500

    Agent related scripts

---
+++ Agent Script/AgentGoToExit.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// On start, this script will get a random exit number from exitlocaitons, and the navmesh agent will set that location as destination.
+/// When reaching the destination, isWalking change to false, the agen stop walking
+/// </summary>
+
+public class AgentGoToExit : MonoBehaviour
+{
+    public ExitLocations exits;
+    private Vector3 exit;
+    private int exitNumber;
+
+    Animator animator;
+
+    NavMeshAgent myNavMeshAgent;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+        GetRandomExit();
+
+    }
+
+    private void GetRandomExit()
+    {
+        exitNumber = Random.Range(0, exits.exitLocations.Count);
+        exit = exits.exitLocations[exitNumber];
+        myNavMeshAgent.speed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myNavMeshAgent.SetDestination(exit);
+        CheckIfReachExit();
+    }
+
+    private void CheckIfReachExit()
+    {
+        if (Vector3.Distance(transform.position, exit) < 1.5f)
+        {
+
+            animator.SetBool("IsWalking", false);
+            myNavMeshAgent.speed = 0f;
+
+        }
+
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if ((this.gameObject.GetComponent<AgentGoToExit>().enabled == true))
+        {
+            Gizmos.DrawLine(transform.position, exit);
+        }
+
+    }
+
+}
---
+++ Agent Script/ExitController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Control the active status of scripts on agent.
+/// When pressing "q", the agent will change their status from follow path to go to exit
+/// </summary>
+public class ExitController : MonoBehaviour
+{
+
+    //public GameObject cube;
+    public GameObject[] cubes;
+
+    void Start()
+    {
+        cubes = GameObject.FindGameObjectsWithTag("people");
+        foreach (GameObject cube in cubes)
+        {
+            cube.gameObject.GetComponent<AgentFollowPath>().enabled = true;
+            cube.gameObject.GetComponent<AgentGoToExit>().enabled = false;
+        }
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+
+            foreach (GameObject cube in cubes)
+            {
+                cube.gameObject.GetComponent<AgentFollowPath>().enabled = false;
+                cube.gameObject.GetComponent<AgentGoToExit>().enabled = true;
+            }
+
+        }
+    }
+}
