12205b91724fe08bbb1d8be88431e8c0911ab188
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2023-08-02 15:43:01 -0400

    Update SimpleCameraController.cs

--- Camera Script/SimpleCameraController.cs
+++ Camera Script/SimpleCameraController.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -6,18 +6,43 @@ using System.Reflection;
 using System;
 
 /// <summary>
-/// This is a simple camera controller that use the Mouse X and Y to rotate the camera
+/// A simple camera script that rotate the camera view,
+/// Have a public function that use a bool to pause the rotation.
+/// The rotation speed can be adjusted by the speed parameter.
 /// </summary>
+
 
 public class SimpleCameraController : MonoBehaviour
 {
+    // Control the direction of the camera
     Vector2 rotation = new Vector2(0, 0);
-    public float speed = 3;
+
+    // Adjust the speed the direction rotation
+    public float speed = 8f;
+
+    // rotateStatus is used to control the rotation of the camera
+    // when camera is not rotation, user can click UI buttons
+    [Header("Press 'q' to turn mouse rotation on/off")]
+    public bool rotateStatus = true;
 
     void Update()
     {
+        if (Input.GetKeyDown("q"))
+        {
+            SwitchRotateStatus();
+        }
+
+        if (rotateStatus)
+        { 
         rotation.y += Input.GetAxis("Mouse X");
         rotation.x += -Input.GetAxis("Mouse Y");
         transform.eulerAngles = (Vector2)rotation * speed;
+        }
     }
+
+    public void SwitchRotateStatus()
+    {
+        rotateStatus = !rotateStatus;
+    }
+
 }
