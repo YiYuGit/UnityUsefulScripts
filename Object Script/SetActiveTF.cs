ac9ad66308e5a55f9cd71473a79a30c2a9ccf2ee
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2023-08-02 15:32:10 -0400

    Update SetActiveTF.cs

--- Object Script/SetActiveTF.cs
+++ Object Script/SetActiveTF.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 using UnityEngine.Video;
 
 /// <summary>
-/// The script is attached to one object that control the active true or false status of other object.
+/// The script is attached to one object that control the active true or false status of other objects.
 /// If you want to control multiple game obejct. Modify the code GameObject to GameObject[]
 /// or put all objects in one empty gameobject.
 /// 
+/// 
+/// Other solutions:
+/// 1.
 /// This solution may not be the best to use if you also need access to the obejects after their active stative is set to false.
 /// In that case, try to use 
 /// 
@@ -16,7 +19,13 @@ using UnityEngine.Video;
 /// rend = GetComponent<Renderer>();
 /// rend.enabled = true/false;
 /// 
+/// 2. 
+/// In case the turn on/off renderer still now working, if still need the renderder to be on but don't want the object to be visible
+///         Put the things that you don't want to show in Main camera into this cullingMask layer in the inspector
+///         Camera.main.cullingMask = ~(1 << LayerMask.NameToLayer("MainCamIgnore"));
+///         
 /// </summary>
+
 public class SetActiveTF : MonoBehaviour
 {
     [Header("Normal Object1")]
@@ -24,19 +33,20 @@ public class SetActiveTF : MonoBehaviour
     [Header("Normal Object2")]
     public GameObject targertObj2;
 
-    [Header("Object with child objects")]
-    public GameObject targertObj3;
+    //[Header("Object with child objects")]
+   // public GameObject targertObj3;
 
+    [Header("Drop multiple objects")]
+    public GameObject[] targertObj4;
 
-
+    // By default, the active status of objects are true
     public bool objActiveStatus = true;
-
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        objActiveStatus = true;
     }
 
     // Update is called once per frame
@@ -54,6 +64,7 @@ public class SetActiveTF : MonoBehaviour
         targertObj2.SetActive(!objActiveStatus);
 
         //turn off/ on the render of the dots so the user won't see
+        /*
         foreach (Renderer r in targertObj3.GetComponentsInChildren<Renderer>())
         {
             r.enabled = !r.enabled;
@@ -64,10 +75,16 @@ public class SetActiveTF : MonoBehaviour
         {
             s.enabled = !s.enabled;
         }
+        */
+
+        // turn off each object in the list
+        foreach (GameObject a in targertObj4)
+        {
+            a.SetActive(!objActiveStatus);
+        }
 
         objActiveStatus = !objActiveStatus;
 
     }
-
 
 }
