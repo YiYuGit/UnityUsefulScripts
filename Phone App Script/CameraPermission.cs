dbac218769599d2cccf8000bba0e4bfa5f71b96d
YiYuGit <68971742+YiYuGit@users.noreply.github.com>
2024-06-11 12:26:53 -0400

    Add files via upload

---
+++ Phone App Script/CameraPermission.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.Android;
+
+public class CameraPermission : MonoBehaviour
+{
+    void Start()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+    }
+}
